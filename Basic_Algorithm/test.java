fa75331363a303dd7b5834ac5d06883c1dd8666f
WeChat098 <99456424+WeChat098@users.noreply.github.com>
2024-07-04 23:15:16 +0800

    ceshi

--- Basic_Algorithm/test.java
+++ Basic_Algorithm/test.java
@@ -3,16 +3,7 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<>();
-        list.add(3);
-        list.add(4);
-        list.add(1);
-        list.addFirst(5);
-        System.out.println(list);
-        list.addLast(23);
-        int a = list.getFirst();
-        System.out.println(a);
-        list.remove();
-        System.out.println(list);
+
+        // 测试
     }
 }
